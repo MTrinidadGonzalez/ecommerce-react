18d830356a0e990db7b345300213165365a5e32d
Trinidad Gonzalez <mtgprimaria@gmail.com>
2023-05-09 15:30:07 -0300

    .

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,6 +1,8 @@
 import { useContext } from "react"
 import { CartContext } from "../context/CartProvider"
 import './cart.css'
+import { IoMdTrash } from "react-icons/io";
+
 function Cart(){
 
     const [cart, setCart]= useContext(CartContext)
@@ -14,12 +16,33 @@ const totalPrice = cart.reduce((acc,curr)=>{
     return acc + curr.quantity * curr.price
 },0)
 
-    return(<>
+const removeItem=(id)=>{
+    setCart((currItems)=>{
+      if(currItems.find((item)=> item.id === id)?.quantity === 1){
+        return currItems.filter((item)=> item.id !== id)
+      }
+      else{
+        return currItems.map((item)=>{
+          if (item.id === id){
+            return {...item, quantity: item.quantity - 1}
+          }
+          else{
+            return item
+          }
+        })
+      }
+    })
+    }
+  
+  
+  
+  return(<>
     <div className="divCArtContainer">
     
         <div className="itemCart">Cantidad de productos: {quantity}</div>
      
-        <div className="itemCart">{cart.map(i=> <p>{i.description}:  ${i.price}</p> )}</div>
+        <div className="itemCart">{cart.map(i=> <p>{i.description}:  ${i.price} <button onClick={()=> removeItem(i.id)}><IoMdTrash/></button></p>  
+        )}</div>
       
         <div className="itemCart">Monto total: $ {totalPrice}</div>
       
